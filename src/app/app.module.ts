fa6e036623b597b66704fb5e8f5d786137780b3e
Abdellatif Oubenmoh <mr.oubenmoh@gmail.com>
2022-06-26 14:18:14 +0100

    added some directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { SingleProductComponent } from './product-list/single-product/single-product.component';
+import { ProductFormComponent } from './product-list/product-form/product-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    ProductListComponent,
+    SingleProductComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
